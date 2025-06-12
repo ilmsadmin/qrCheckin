db526e86838b8a83a9277582dbf2fd615c7b154d
adminilms <admin@ilms.vn>
2025-06-12 11:36:27 +0700

    setup ios, android

---
+++ ios-app/qrCheckin/qrCheckinTests/qrCheckinTests.swift
@@ -0,0 +1,17 @@
+//
+//  qrCheckinTests.swift
+//  qrCheckinTests
+//
+//  Created by toan on 12/6/25.
+//
+
+import Testing
+@testable import qrCheckin
+
+struct qrCheckinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
